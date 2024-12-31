d4e2e2ca1af91c509df2cebe7c111997881a3be1
Irash Perera <irash.21@cse.mrt.ac.lk>
2024-12-31 12:58:49 +0530

    feat: update routing to include Home and NewTransaction pages, and adjust Dashboard path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/LogIn"
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import NewTransaction from "./pages/NewTransaction";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Dashboard/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/new-transaction" element={<NewTransaction />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
